d2f51852250f15313c362d473e89012add75d5d1
kbetkowsky <92058076+kbetkowsky@users.noreply.github.com>
2026-03-14 18:29:37 +0100

    Add additional allowed origin for CORS

--- induo-house/src/main/java/com/induohouse/induo_house/config/SecurityConfig.java
+++ induo-house/src/main/java/com/induohouse/induo_house/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.75.225.78:3000"));
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
